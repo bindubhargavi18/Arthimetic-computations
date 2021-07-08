72e89ba9e310dd62d0d4cfd4af6327b14e3ac48e
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-08 20:54:11 +0530

    Added:storing values into dictionary

--- arthimetic.sh
+++ arthimetic.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
+declare -A results
 read -p "Enter a value:" a
 read -p "Enter b value:" b
 read -p "Enter c value:" c
 echo $a,$b,$c
-
-result=$((a%b+c))
-
-echo $result
+r1=$((a+b*c))
+r2=$((a*b+c))
+r3=$((c+a/b))
+r4=$((a%b+c))
+results[comp1]="$r1"
+results[comp2]="$r2"
+results[comp3]="$r3"
+results[comp4]="$r4"
+echo "All keys:" ${!results[@]}
+echo "all results:" ${results[@]}
